66ecbdb20239f549a790366d056a825dd508122a
Kara Cavanaugh <karacav@msn.com>
2023-11-30 19:08:27 -0800

    hooked sprite sheet up to dropdown options

--- script.js
+++ script.js
@@ -1,27 +1,89 @@
+let playerState = 'sit';
+let dropdown = document.querySelector("#animations");
+dropdown.addEventListener("change", function (e) {
+    playerState = e.target.value;
+    console.log(playerState);
+})
+
 const canvas = document.getElementById("canvas1");
 const ctx = canvas.getContext("2d");
 const canvas_height = canvas.height = 600;
 const canvas_width = canvas.width = 600;
 
-
 const playerImg = new Image();
-playerImg.src = '/shadow_dog.png';
+playerImg.src = './shadow_dog.png';
 const spriteWidth = 575;
 const spriteHeight = 523;
-let frameX = 0;
-let frameY = 0;
 let gameFrame = 0;
 const staggerFrames = 7;
+
+const spriteAnimations = [];
+const animationStates = [
+    {
+        name: 'idle',
+        frames: 7,
+    },
+    {
+        name: 'jump',
+        frames: 7,
+    },
+    {
+        name: 'fall',
+        frames: 7,
+    },
+    {
+        name: 'run',
+        frames: 7,
+    },
+    {
+        name: 'dizzy',
+        frames: 11,
+    },
+    {
+        name: 'sit',
+        frames: 5,
+    },
+    {
+        name: 'roll',
+        frames: 7,
+    },
+    {
+        name: 'bite',
+        frames: 7,
+    },
+    {
+        name: 'ko',
+        frames: 12,
+    },
+    {
+        name: 'getHit',
+        frames: 4,
+    }
+
+];
+animationStates.forEach((action, index) => {
+    let frames = {
+        loc: [],
+    }
+    for (let j = 0; j < action.frames; j++) {
+        let positionX = j * spriteWidth;
+        let positionY = index * spriteHeight;
+        frames.loc.push({ x: positionX, y: positionY });
+    }
+    spriteAnimations[action.name] = frames;
+});
+console.log(spriteAnimations);
 
 // LEFT OFF HERE https://youtu.be/GFO_txvwK_c?si=t35fiqiSmLUPd0cC&t=1680 (28 min)
 
 function animate() {
     ctx.clearRect(0, 0, canvas_width, canvas_height);
+
+    let position = Math.floor(gameFrame / staggerFrames) % spriteAnimations[playerState].loc.length;
+    let frameX = spriteWidth * position;
+    let frameY = spriteAnimations[playerState].loc[position].y;
+
     ctx.drawImage(playerImg, frameX, frameY, spriteWidth, spriteHeight, 0, 450, 150, 150);
-
-    let position = Math.floor(gameFrame / staggerFrames) % 6;
-    frameX = spriteWidth * position;
-
     gameFrame++;
     requestAnimationFrame(animate);
 }
@@ -29,11 +91,11 @@ animate();
 
 
 
-let startMessage = document.querySelector("#start");
+// let startMessage = document.querySelector("#start");
 
-function onStart() {
-    startMessage.textContent = "PAUSED";
-}
+// function onStart() {
+//     startMessage.textContent = "PAUSED";
+// }
 
 
 
